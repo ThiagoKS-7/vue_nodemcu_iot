49edfd54fedd2c64e4f99ed1f9d1e242fb60577a
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-07-20 00:31:03 -0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue';
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(CarbonComponentsVue).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <cv-text-input
+      label="Who are you?"
+      v-model="yourName"
+      placeholder="your name"
+    />
+    <cv-button @click="onClick">Hello {{yourName}}</cv-button>
+    <cv-modal :visible="visible" @modal-hidden="modalClosed">
+      <template slot="title">Welcome to @carbon/vue {{yourName}}</template>
+      <template slot="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, seed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </template>
+    </cv-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  data() {
+      return {
+        yourName: '',
+        visible: false,
+      };
+    },
+    methods: {
+      onClick() {
+        this.visible = true;
+      },
+      modalClosed() {
+        this.visible = false;
+      },
+    },
 });
 </script>
+<style>
+  .sample {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 600px;
+    margin: 5% auto;
+  }
+
+  .cv-text-input {
+    margin: 30px 0;
+  }
+</style>
